eb2d573a362fc4f41636847f0eb31c999eabc63e
Cano <developer.sasazono@gmail.com>
2024-01-17 13:39:21 +0900

    support remote user

--- packages/react-misskey/src/components/mfm_mention.tsx
+++ packages/react-misskey/src/components/mfm_mention.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import type mfm from "mfm-js";
+import { toUnicode } from "punycode";
 import { FC } from "react";
 import styles from "./mfm_mention.module.css";
 
@@ -9,18 +10,26 @@ type Props = {
   isMe?: boolean;
 };
 export const MfmMention: FC<Props> = ({ node, host, isMe }) => {
-  const userUrl = new URL(node.props.acct, `https://${host}`);
-  const avatarUrl = new URL(`avatar/${node.props.acct}`, `https://${host}`);
+  const isRemote = node.props.host && host !== node.props.host;
+  const remoteHost = isRemote ? node.props.host : null;
+
+  const acct = remoteHost
+    ? `@${node.props.username}@${toUnicode(remoteHost)}`
+    : `@${node.props.username}`;
+
+  const hostUrl = new URL(`https://${host}`);
+  const url = new URL(acct, hostUrl);
+  const avatarUrl = new URL(`avatar/${node.props.acct}`, hostUrl);
 
   return (
     <a
-      href={userUrl.toString()}
+      href={url.toString()}
       target="_blank"
       rel="noreferrer"
       className={clsx(styles.root, isMe && styles.isMe)}
     >
       <img src={avatarUrl.toString()} alt="" className={styles.img} />
-      <span>{node.props.acct}</span>
+      <span>{acct}</span>
     </a>
   );
 };
--- packages/react-misskey/stories/mfm_mention.stories.tsx
+++ packages/react-misskey/stories/mfm_mention.stories.tsx
@@ -19,7 +19,51 @@ type Story = StoryObj<typeof MfmMention>;
 
 export const Default: Story = {
   args: {
-    node: { type: "mention", props: { acct: "@ai", host: null, username: "" } },
+    node: {
+      type: "mention",
+      props: { acct: "@ai", host: null, username: "ai" },
+    },
+    host: "misskey.io",
+    isMe: false,
+  },
+};
+
+export const IsMe: Story = {
+  args: {
+    node: {
+      type: "mention",
+      props: { acct: "@ai", host: null, username: "ai" },
+    },
+    host: "misskey.io",
+    isMe: true,
+  },
+};
+
+export const Remote: Story = {
+  args: {
+    node: {
+      type: "mention",
+      props: {
+        acct: "@repo@misskey.test",
+        host: "misskey.test",
+        username: "repo",
+      },
+    },
+    host: "misskey.io",
+    isMe: false,
+  },
+};
+
+export const RemoteUnicode: Story = {
+  args: {
+    node: {
+      type: "mention",
+      props: {
+        acct: "@ai@xn--931a.test",
+        host: "xn--931a.test",
+        username: "ai",
+      },
+    },
     host: "misskey.io",
     isMe: false,
   },
