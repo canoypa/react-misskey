16e6cf0538dcf9b170d7045b8c7e4bfa59b5e182
Cano <developer.sasazono@gmail.com>
2024-01-17 14:49:09 +0900

    link

--- packages/react-misskey/src/components/mfm.tsx
+++ packages/react-misskey/src/components/mfm.tsx
@@ -1,4 +1,5 @@
 import { MfmHashtag } from "./mfm_hashtag";
+import { MfmLink } from "./mfm_link";
 import { MfmMention } from "./mfm_mention";
 
-export { MfmHashtag, MfmMention };
+export { MfmHashtag, MfmLink, MfmMention };
