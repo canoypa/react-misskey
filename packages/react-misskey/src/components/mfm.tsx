f5c44b47b879ee630c5c319c5def11cb4a5a01f8
Cano <developer.sasazono@gmail.com>
2024-01-17 16:52:13 +0900

    wip mfm renderer

--- packages/react-misskey/src/components/mfm.tsx
+++ packages/react-misskey/src/components/mfm.tsx
@@ -1,6 +1,23 @@
+import type mfm from "mfm-js";
+import { toString as toMfmString } from "mfm-js";
 import { MfmEmojiCode } from "./mfm_emoji_code";
 import { MfmHashtag } from "./mfm_hashtag";
 import { MfmLink } from "./mfm_link";
 import { MfmMention } from "./mfm_mention";
+import { MfmText } from "./mfm_text";
 
 export { MfmEmojiCode, MfmHashtag, MfmLink, MfmMention };
+
+export const renderNode = (node: mfm.MfmNode) => {
+  switch (node.type) {
+    case "text":
+      return <MfmText node={node} />;
+    default:
+      // const check: never = node;
+      return toMfmString(node);
+  }
+};
+
+export const renderNodes = (nodes: mfm.MfmNode[]) => {
+  return nodes.map(renderNode);
+};
